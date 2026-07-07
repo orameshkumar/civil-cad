873fb80f75ea11eaf6a04e87e9d03cb917fa9fb1
orameshkumar <59666060+orameshkumar@users.noreply.github.com>
2026-07-07 02:32:32 +0000

    Deploying to gh-pages from @ orameshkumar/civil-cad@82f8e0889fc6559a4f9b4de584e48e61c83cc299 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/civil-cad/sw.js', { scope: '/civil-cad/' })})}
